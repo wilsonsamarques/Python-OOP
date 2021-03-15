ddf7d7fec4d11139d30bb9189fd95c3f91fcb85a
wilsonsamarques <wilsonsamarques@gmail.com>
2021-03-14 21:16:55 -0300

    Lecture 1

---
+++ Classes_and_Instances.py
@@ -0,0 +1,24 @@
+# data -> attributes
+# functions -> methods
+# Let's create a class to represent an employee in a company.
+
+# class -> blueprint for creating instances
+# each unique employee will be an instances of that class
+
+
+
+
+class Employee:
+    pass
+
+emp_1 = Employee()
+emp_2 = Employee()
+
+print(emp_1)
+print(emp_2)
+
+
+
+
+
+
